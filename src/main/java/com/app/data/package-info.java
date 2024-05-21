cd50dc59c7263e7ff5f65be38f538070a211b6b0
start.vaadin.com <no-reply@vaadin.com>
2024-05-21 03:06:46 +0000

    Generated project

---
+++ src/main/java/com/app/data/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.app.data;
+
+import org.springframework.lang.NonNullApi;
---
+++ src/main/java/com/app/views/dashboard/DashboardView.java
@@ -0,0 +1,33 @@
+package com.app.views.dashboard;
+
+import com.app.views.MainLayout;
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.dependency.Uses;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+
+@PageTitle("Dashboard")
+@Route(value = "", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+@Uses(Icon.class)
+public class DashboardView extends Composite<VerticalLayout> {
+
+    public DashboardView() {
+        VerticalLayout layoutColumn2 = new VerticalLayout();
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        layoutColumn2.setWidth("100%");
+        layoutColumn2.getStyle().set("flex-grow", "1");
+        layoutRow.addClassName(Gap.MEDIUM);
+        layoutRow.setWidth("100%");
+        layoutRow.setHeight("min-content");
+        getContent().add(layoutColumn2);
+        getContent().add(layoutRow);
+    }
+}
